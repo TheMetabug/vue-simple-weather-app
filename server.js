d9577d355760101acde2a126d165777b522e91ca
TheMetabug <metabug@gmail.com>
2020-01-28 15:32:31 +0200

    Started to implement Vue/js support to read OpenWeatherMap JSON file.

--- server.js
+++ server.js
@@ -1,15 +1,45 @@
 var express = require('express')
-var vue = require('vue');
-var nodeSchedule = require('node-schedule');
-
+var nodeSchedule = require('node-schedule')
+var fs = require('fs')
 const port = 8080
 const app = express()
+/**
+ * Function what fetches the data from OpenWeatherMap API and writes it to file.
+ */
+var fetchAndCreateJSON = function() {
+    var obj = {
+        table: []
+    }
+    obj.table.push({data: "asd"})
+    var json = JSON.stringify(obj)
+    fs.writeFile(__dirname + '/data/weatherDataDynamic.json', json, 'utf8', (err) => {
+        if (err) throw err;
+        console.log("File has been saved!")
+    })
+}
 
+/**
+ * Static file initialisations
+ */
 app.use(express.static('static'))
-app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
-app.use('/vue', express.static(__dirname + '/node_modules/vue/dist/'));
-app.use('/bootstrap', express.static(__dirname + '/bootstrap/dist/'));
-app.use('/bootstrap-vue', express.static(__dirname + '/bootstrap-vue/dist/'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'))
+app.use('/vue', express.static(__dirname + '/node_modules/vue/dist/'))
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'))
 
-app.get('/', (req, res) => res.send('index.html'))
+/**
+ * GET requests
+ */
+app.get('/', function(req, res) {
+    res.render('index.html')
+})
+app.get('/weatherdata', function(req, res) {
+    var jsonFile = fs.readFileSync(__dirname + '/data/weatherData.json')
+    var jsonData = JSON.parse(jsonFile)
+    fetchAndCreateJSON()
+    res.json(jsonData)
+})
+
+/**
+ * Listen command
+ */
 app.listen(port, () => console.log('Application listening on port: ' + port))
--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,60 @@
 var doc = $(document).ready(function() {
-    console.log("Script initialized!")
+    // Weather icon url information
+    var weatherIconUrl      = 'http://openweathermap.org/img/wn/'
+    var weatherIconFilename = '@2x.png'
 
+    // Test weather data (placeholder)
+    var testWeather = {
+        cityname: "Helsinki",
+        description: "rainy sky",
+        current: {
+            dateTime: 1560350191,
+            icon: "10d",
+            temp: 12,
+            humidity: 23,
+            windspeed: 0.77
+        },
+    }
+
+    // Vue App
     var app = new Vue({
         el: '#app',
         data: {
-            message: 'Hello'
+            message: 'Hello',
+            searchDisabled: false,
+            weatherData: testWeather,
+            weatherIcon: weatherIconUrl + testWeather.current.icon + weatherIconFilename
         },
         methods: {
             printMessage: function() {
                 console.log(this.message)
+                var parameters = { city: "helsinki" }
+                var self = this
+                $.ajax({
+                    url: '/weatherdata',
+                    data: parameters,
+                    beforeSend: function(xhr) {
+                        this.searchDisabled = true
+                    },
+                    success: function(result, xhr, status) {
+                        self.parseWeatherData(result)
+                    },
+                    error: function(xhr, status, error) {
+                        console.log(status);
+                        console.log(error);
+                    },
+                    complete: function(xhr, status) {
+                        this.searchDisabled = false
+                    }
+                })
+            },
+            parseWeatherData: function(jsonData) {
+                testWeather.cityname            = jsonData.name,
+                testWeather.description         = jsonData.weather[0].description,
+                testWeather.current.icon        = this.weatherIconUrl + jsonData.weather.icon,
+                testWeather.current.temp        = jsonData.main.temp,
+                testWeather.current.humidity    = jsonData.main.humidity,
+                testWeather.current.windspeed   = jsonData.wind.speed
             }
         }
     });
